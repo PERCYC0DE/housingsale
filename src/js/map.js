65a27e4b8828e769814ce12cd8b341e1262646bf
Percy Tejada <percytejada.code@gmail.com>
2022-12-12 21:15:02 -0500

    Creating models for Property, Category and Price

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,11 @@
 (function () {
-  const lat = 20.67444163271174;
-  const lng = -103.38739216304566;
+  const lat = -13.4164325;
+  const lng = -76.1397925;
   const map = L.map("map").setView([lat, lng], 16);
   let marker;
 
+  // Use provider and geocoder
+  const geocodeService = L.esri.Geocoding.geocodeService();
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -14,4 +16,26 @@
     draggable: true,
     autoPan: true,
   }).addTo(map);
+
+  // Detect movement of the pin and read its coordinate
+  marker.on("moveend", function (e) {
+    marker = e.target;
+    const position = marker.getLatLng();
+    map.panTo(new L.LatLng(position.lat, position.lng));
+
+    // Get info of street to draggable pin
+    geocodeService
+      .reverse()
+      .latlng(position, 13)
+      .run(function (error, result) {
+        marker.bindPopup(result.address.LongLabel).openPopup();
+
+        document.querySelector(".street").textContent =
+          result?.address?.Address ?? "";
+        document.querySelector("#street").value =
+          result?.address?.Address ?? "";
+        document.querySelector("#lat").value = result?.latlng?.lat ?? "";
+        document.querySelector("#lng").value = result?.latlng?.lng ?? "";
+      });
+  });
 })();
